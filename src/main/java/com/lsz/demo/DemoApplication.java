ac5a1c4282683f745ae0640c791113a2c28ab05c
lsz <connorlsz@163.com>
2020-09-18 14:51:48 +0800

    提交后回退

--- src/main/java/com/lsz/demo/DemoApplication.java
+++ src/main/java/com/lsz/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
-
+asdfasd
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/lsz/demo/dao/PersonDao.java
+++ src/main/java/com/lsz/demo/dao/PersonDao.java
@@ -5,6 +5,6 @@ import com.lsz.demo.entity.Person;
 import java.util.List;
 
 public interface PersonDao {
-
+555
     List<Person> queryPersonList();
 }
